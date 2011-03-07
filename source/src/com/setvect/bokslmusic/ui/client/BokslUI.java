cddbd722382761bb43760d443fabdffe846b453d
setvect@naver.com <setvect@naver.com@76c2b2f6-976e-2fa4-fefe-f2a6046cae85>
2011-03-07 08:34:42 +0000

    git-svn-id: http://literatureboy.googlecode.com/svn/BokslMusic@219 76c2b2f6-976e-2fa4-fefe-f2a6046cae85

--- source/src/com/setvect/bokslmusic/ui/client/BokslUI.java
+++ source/src/com/setvect/bokslmusic/ui/client/BokslUI.java
@@ -1,10 +1,14 @@
 package com.setvect.bokslmusic.ui.client;
 
+import com.extjs.gxt.ui.client.Style.LayoutRegion;
+import com.extjs.gxt.ui.client.util.Margins;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.Label;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
+import com.extjs.gxt.ui.client.widget.Text;
 import com.extjs.gxt.ui.client.widget.Viewport;
-import com.extjs.gxt.ui.client.widget.layout.RowLayout;
+import com.extjs.gxt.ui.client.widget.layout.BorderLayout;
+import com.extjs.gxt.ui.client.widget.layout.BorderLayoutData;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
 
@@ -12,19 +16,42 @@ import com.google.gwt.user.client.ui.RootPanel;
  * Entry point classes define <CODE>onModuleLoad()</CODE>.
  */
 public class BokslUI implements EntryPoint {
-	Viewport viewport = new Viewport();
-
 	public void onModuleLoad() {
-
-		LayoutContainer warp = new LayoutContainer(new RowLayout());
+		Viewport warp = new Viewport();
+		warp.setLayout(new BorderLayout());
 		warp.setBorders(true);
+		LayoutContainer hearder = header();
+		warp.add(hearder, new BorderLayoutData(LayoutRegion.NORTH, 30));
+		warp.add(body(), new BorderLayoutData(LayoutRegion.CENTER));
+		RootPanel rootPanel = RootPanel.get();
+		rootPanel.add(warp);
+	}
 
+	private LayoutContainer body() {
+		LayoutContainer body = new LayoutContainer();
+
+		body.setLayout(new BorderLayout());
+		ContentPanel northPanel = new ContentPanel();
+		northPanel.add(new Text("11111111111"));
+		BorderLayoutData northData = new BorderLayoutData(LayoutRegion.NORTH, 100, 50, 200);
+		northData.setCollapsible(true);
+		northData.setMargins(new Margins(5, 5, 5, 5));
+		body.add(northPanel, northData);
+
+		ContentPanel centerPanel = new ContentPanel();
+		centerPanel.add(new Text("22222222222"));
+		BorderLayoutData centerData = new BorderLayoutData(LayoutRegion.CENTER, 100, 50, 200);
+		centerData.setCollapsible(true);
+		centerData.setMargins(new Margins(5, 5, 5, 5));
+		body.add(centerPanel, centerData);
+
+		return body;
+	}
+
+	private LayoutContainer header() {
 		LayoutContainer hearder = new LayoutContainer();
 		Label titleHeader = new Label("복슬 Music");
 		hearder.add(titleHeader);
-		warp.add(hearder);
-
-		RootPanel rootPanel = RootPanel.get();
-		rootPanel.add(warp);
+		return hearder;
 	}
 }
