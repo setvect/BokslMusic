8c1789f38ad2bb187eed1b0bf82dbdee8024c200
setvect@naver.com <setvect@naver.com@76c2b2f6-976e-2fa4-fefe-f2a6046cae85>
2011-03-16 08:34:31 +0000

    git-svn-id: http://literatureboy.googlecode.com/svn/BokslMusic@227 76c2b2f6-976e-2fa4-fefe-f2a6046cae85

--- source/src/com/setvect/bokslmusic/ui/client/BokslUI.java
+++ source/src/com/setvect/bokslmusic/ui/client/BokslUI.java
@@ -1,15 +1,16 @@
 package com.setvect.bokslmusic.ui.client;
 
 import com.extjs.gxt.ui.client.widget.ContentPanel;
-import com.extjs.gxt.ui.client.widget.HorizontalPanel;
-import com.extjs.gxt.ui.client.widget.Label;
-import com.extjs.gxt.ui.client.widget.Text;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -19,12 +20,19 @@ import com.google.gwt.user.client.ui.Widget;
  */
 public class BokslUI implements EntryPoint {
 	public void onModuleLoad() {
-		VerticalPanel warp = new VerticalPanel();
-		warp.add(headerPannel());
-		warp.add(syncPannel());
-		warp.add(playPannel());
-		warp.add(listPannel());
-		warp.setSize("100%", "");
+
+		HorizontalPanel warp = new HorizontalPanel();
+		warp.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+		warp.setWidth("100%");
+
+		VerticalPanel body = new VerticalPanel();
+		warp.add(body);
+		body.setWidth("95%");
+		body.add(headerPannel());
+		body.add(syncPannel());
+		body.add(playPannel());
+		body.add(listPannel());
+
 		RootPanel rootPanel = RootPanel.get();
 		rootPanel.add(warp);
 	}
@@ -40,9 +48,17 @@ public class BokslUI implements EntryPoint {
 		sync.setHeading("동길화 설정");
 
 		HorizontalPanel syncHori = new HorizontalPanel();
-		syncHori.setTableWidth("100%");
+		sync.add(syncHori);
+
+		syncHori.setBorderWidth(1);
+		syncHori.setWidth("100%");
+		
 		VerticalPanel syncHoriVerty1 = new VerticalPanel();
+		syncHori.add(syncHoriVerty1);
+		syncHori.setCellWidth(syncHoriVerty1, "420");
+
 		syncHoriVerty1.add(new HTML("동기화목록"));
+
 		FlowPanel syncHoriVerty1Top = new FlowPanel();
 		Button syncHoriVerty1TopBtn1 = new Button("DB동기화");
 		Button syncHoriVerty1TopBtn2 = new Button("전체 폴더 동기화");
@@ -58,13 +74,17 @@ public class BokslUI implements EntryPoint {
 		syncHoriVerty1Bottom.add(syncHoriVerty1BottomText);
 		syncHoriVerty1Bottom.add(syncHoriVerty1BottomBtn);
 		syncHoriVerty1.add(syncHoriVerty1Bottom);
-		syncHori.add(syncHoriVerty1);
 
 		VerticalPanel syncHoriVerty2 = new VerticalPanel();
-		syncHoriVerty2.add(new HTML("메시지로그"));
-		syncHoriVerty2.add(new HTML("메시지로그 내용"));
 		syncHori.add(syncHoriVerty2);
-		sync.add(syncHori);
+		syncHoriVerty2.setWidth("100%");
+		syncHoriVerty2.add(new HTML("메시지로그"));
+
+		ScrollPanel syncHoriVerty2Scroll = new ScrollPanel(new HTML(
+				"메<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>시지로그 내용"));
+		syncHoriVerty2Scroll.setHeight("200px");
+		syncHoriVerty2.add(syncHoriVerty2Scroll);
+
 		return sync;
 	}
 
@@ -72,15 +92,18 @@ public class BokslUI implements EntryPoint {
 		ContentPanel play = new ContentPanel();
 		play.setCollapsible(true);
 		play.setHeading("음악재생");
-		play.add(new Text("재생 패널"));
+		TextBox txt = new TextBox();
+		play.add(txt);
+		txt.setText("재생 패널");
 		return play;
 	}
 
 	private Widget listPannel() {
 		ContentPanel list = new ContentPanel();
 		list.setCollapsible(true);
-		list.add(new Text("리스트 패널"));
+		TextBox txt = new TextBox();
+		list.add(txt);
+		txt.setText("재생 패널");
 		return list;
 	}
-
 }
