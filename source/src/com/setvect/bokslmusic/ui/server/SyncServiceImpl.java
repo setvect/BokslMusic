08d8965cd047938973ad30cb3efaec6aa6995c83
setvect@naver.com <setvect@naver.com@76c2b2f6-976e-2fa4-fefe-f2a6046cae85>
2011-03-24 02:02:03 +0000

    git-svn-id: http://literatureboy.googlecode.com/svn/BokslMusic@243 76c2b2f6-976e-2fa4-fefe-f2a6046cae85

--- source/src/com/setvect/bokslmusic/ui/server/SyncServiceImpl.java
+++ source/src/com/setvect/bokslmusic/ui/server/SyncServiceImpl.java
@@ -3,12 +3,20 @@ package com.setvect.bokslmusic.ui.server;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.setvect.bokslmusic.service.music.MusicService;
 import com.setvect.bokslmusic.ui.client.SyncService;
 import com.setvect.bokslmusic.ui.shared.model.MusicDirectoryModel;
 
 @SuppressWarnings("serial")
+@Controller
 public class SyncServiceImpl extends RemoteServiceServlet implements SyncService {
+	@Autowired
+	private MusicService musicService;
+
 	public List<MusicDirectoryModel> getSyncList() throws IllegalArgumentException {
 		List<MusicDirectoryModel> list = new ArrayList<MusicDirectoryModel>();
 		MusicDirectoryModel a = new MusicDirectoryModel();
