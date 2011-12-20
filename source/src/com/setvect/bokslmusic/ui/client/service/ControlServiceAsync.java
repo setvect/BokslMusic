f27d8056f6f5675cb6184b4c516f14f9b0318dad
setvect@naver.com <setvect@naver.com@76c2b2f6-976e-2fa4-fefe-f2a6046cae85>
2011-12-20 11:33:34 +0000

    GWT 설정 삭제
    git-svn-id: http://literatureboy.googlecode.com/svn/BokslMusic@327 76c2b2f6-976e-2fa4-fefe-f2a6046cae85

--- source/src/com/setvect/bokslmusic/ui/client/service/ControlServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package com.setvect.bokslmusic.ui.client.service;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface ControlServiceAsync {
-
-	void echo(String message, AsyncCallback<String> callback);
-
-}
