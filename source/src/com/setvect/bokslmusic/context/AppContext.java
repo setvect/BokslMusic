30844ff5b60ca81cd84f872e76a65ec65674fac2
setvect <setvect@naver.com>
2012-11-25 10:25:38 +0900

    Spring 3.1 변환 작업중...

--- source/src/com/setvect/bokslmusic/context/AppContext.java
+++ source/src/com/setvect/bokslmusic/context/AppContext.java
@@ -1,11 +1,78 @@
 package com.setvect.bokslmusic.context;
 
+import static org.springframework.context.annotation.FilterType.ANNOTATION;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.orm.hibernate3.HibernateTransactionManager;
+import org.springframework.orm.hibernate3.annotation.AnnotationSessionFactoryBean;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
+import com.setvect.bokslmusic.vo.code.Code;
+import com.setvect.bokslmusic.vo.music.Album;
+import com.setvect.bokslmusic.vo.music.MusicArticle;
+import com.setvect.bokslmusic.vo.music.MusicDirectory;
+import com.setvect.bokslmusic.vo.music.PlayItem;
+import com.setvect.bokslmusic.vo.music.PlayTime;
 
 @Configuration
 @ImportResource({ "classpath:/config/applicationContext.xml" })
-@ComponentScan(basePackages = "com.setvect.bokslmusic")
+@ComponentScan(basePackages = "com.setvect.bokslmusic", useDefaultFilters = false, 
+	includeFilters = { @Filter(type = ANNOTATION, value = Service.class) }, 
+	excludeFilters = @Filter(type = ANNOTATION, value = Controller.class)
+)
 public class AppContext {
+
+	@Autowired
+	private AnnotationSessionFactoryBean sessionFactory;
+
+	@Bean
+	public AnnotationSessionFactoryBean sessionFactory() {
+		AnnotationSessionFactoryBean sessionFactory = new AnnotationSessionFactoryBean();
+
+		Properties hibernateProperties = new Properties();
+		hibernateProperties.setProperty("hibernate.connection.driver_class", "org.h2.Driver");
+		hibernateProperties.setProperty("hibernate.connection.url", "jdbc:h2:D:/work/git/BokslMusic/db/boksl_music");
+		hibernateProperties.setProperty("hibernate.connection.username", "sa");
+		hibernateProperties.setProperty("hibernate.connection.password", "");
+		hibernateProperties.setProperty("hibernate.connection.poolsize", "20");
+		hibernateProperties.setProperty("hibernate.cache.provider_class", "org.hibernate.cache.EhCacheProvider");
+		hibernateProperties.setProperty("hibernate.cache.use_query_cache", "true");
+		hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		hibernateProperties.setProperty("current_session_context_class", "thread");
+		hibernateProperties.setProperty("hibernate.show_sql", "false");
+		hibernateProperties.setProperty("hibernate.format_sql", "true");
+		hibernateProperties.setProperty("hibernate.connection.shutdown", "true");
+		sessionFactory.setHibernateProperties(hibernateProperties);
+
+		Class[] vo = new Class[] { MusicDirectory.class, MusicArticle.class, Album.class, PlayItem.class,
+				PlayTime.class, Code.class };
+
+		sessionFactory.setAnnotatedClasses(vo);
+
+		return sessionFactory;
+	}
+
+	@Bean
+	public HibernateTemplate hibernateTemplate() {
+		HibernateTemplate bean = new HibernateTemplate();
+		bean.setSessionFactory(sessionFactory.getObject());
+		return bean;
+	}
+
+	@Bean
+	public HibernateTransactionManager hibernateTxManager() {
+		HibernateTransactionManager bean = new HibernateTransactionManager();
+		bean.setSessionFactory(sessionFactory.getObject());
+		return bean;
+	}
+
 }
---
+++ source/src/com/setvect/bokslmusic/context/WebContext.java
@@ -0,0 +1,55 @@
+package com.setvect.bokslmusic.context;
+
+import static org.springframework.context.annotation.FilterType.ANNOTATION;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.annotation.DefaultAnnotationHandlerMapping;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import com.setvect.bokslmusic.web.SessionCheckInterceptor;
+import com.setvect.common.http.MultiFileCommonsMultipartResolver;
+
+@Configuration
+@ImportResource({ "classpath:/config/applicationWeb.xml" })
+@ComponentScan(basePackages = "com.setvect.bokslmusic.web", useDefaultFilters = false, includeFilters = @Filter(type = ANNOTATION, value = Controller.class), excludeFilters = @Filter(type = ANNOTATION, value = Service.class))
+public class WebContext {
+	@Autowired
+	private SessionCheckInterceptor sessionCheckerInterceptor;
+
+	@Bean
+	public SessionCheckInterceptor loginCheckInterceptor() {
+		return new SessionCheckInterceptor();
+	}
+
+	@Bean
+	public DefaultAnnotationHandlerMapping defaultAnnotationHandlerMapping() {
+		DefaultAnnotationHandlerMapping bean = new DefaultAnnotationHandlerMapping();
+		bean.setInterceptors(new Object[] { sessionCheckerInterceptor });
+		return bean;
+	}
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setViewClass(JstlView.class);
+		bean.setPrefix("/");
+		bean.setSuffix(".jsp");
+		return bean;
+	}
+
+	@Bean
+	public MultiFileCommonsMultipartResolver multipartResolver() {
+		MultiFileCommonsMultipartResolver bean = new MultiFileCommonsMultipartResolver();
+		bean.setMaxUploadSize(10000000);
+		return bean;
+	}
+
+}
